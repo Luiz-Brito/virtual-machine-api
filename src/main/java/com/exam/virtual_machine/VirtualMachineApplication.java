a94cfa3e8cdcba52fcd390230591f5bfbaa96674
Luiz Brito <luizbsilva4@gmail.com>
2025-06-06 10:43:31 -0300

    first commit

---
+++ src/main/java/com/exam/virtual_machine/VirtualMachineApplication.java
@@ -0,0 +1,13 @@
+package com.exam.virtual_machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/virtual_machine/VirtualMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.virtual_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
